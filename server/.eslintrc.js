74ef5b247fe3c8a86c18f02f04258f51a810c084
Igor Mongault <mongaultigor@gmail.com>
2021-11-03 04:20:20 -0400

    Setup express js server

---
+++ server/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
